3e7f2fabcd74eed08eca5a9bab581450a74348aa
NoobSaiyan <ms19961998@gmail.com>
2020-07-21 16:29:24 +0530

    completed animation

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String animation = 'go';
+
+  void _incrementCounter() {
+    setState(() {
+      if (animation == 'go') {
+        animation = 'come';
+      } else {
+        animation = 'go';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 300.0,
+          height: 300.0,
+          child: FlareActor(
+            'assets/starBoy.flr',
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: animation,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
